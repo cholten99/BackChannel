ad277a7f32ce85a64db1076e55c8e020e3056eb4
David Durant <dave@bowsy.co.uk>
2013-05-08 04:49:31 +0100

    Double database connection success

--- backchannel/wsgi.py
+++ backchannel/wsgi.py
@@ -1,50 +1,54 @@
 #!/usr/bin/env python
 
-
 import time
 import sys
 import os
 import json
+import pprint
 from flask import Flask, Request, Response
+import MySQLdb
 application = app = Flask('wsgi')
 
 @app.route('/')
 def welcome():
-    return 'Fishy fishy fish!'
+  return "Today's fish!"
 
 @app.route('/env')
 def env():
-    return os.environ.get("VCAP_SERVICES", "{}")
+  services_data = os.environ.get("VCAP_SERVICES")
+  if (not services_data):
+    return "Local"
+  else:
+    return services_data
 
-@app.route('/mongo')
-def mongotest():
-    from pymongo import Connection
-    uri = mongodb_uri()
-    conn = Connection(uri)
-    coll = conn.db['ts']
-    coll.insert(dict(now=int(time.time())))
-    last_few = [str(x['now']) for x in coll.find(sort=[("_id", -1)], limit=10)]
-    body = "\n".join(last_few)
-    return Response(body, content_type="text/plain;charset=UTF-8")
+@app.route('/mysql_test')
+def mysql_test():
 
-def mongodb_uri():
-    local = os.environ.get("MONGODB", None)
-    if local:
-        return local
-    services = json.loads(os.environ.get("VCAP_SERVICES", "{}"))
-    if services:
-        creds = services['mongodb-1.8'][0]['credentials']
-        uri = "mongodb://%s:%s@%s:%d/%s" % (
-            creds['username'],
-            creds['password'],
-            creds['hostname'],
-            creds['port'],
-            creds['db'])
-        print >> sys.stderr, uri
-        return uri
+  con = mysql_connect()
+
+  try:
+    con.query("SELECT VERSION()")
+    result = con.use_result()
+    return "MySQL version: %s" % result.fetch_row()[0]
+
+  except:
+    return "Unexpected error:", sys.exc_info()[0]
+
+  finally:
+    if con:
+      con.close()
+
+def mysql_connect():
+  try:
+    if (env() == "Local"):
+      return MySQLdb.connect("localhost","test","test","test")
     else:
-        raise Exception, "No services configured"
-    
+      services = json.loads(os.environ.get("VCAP_SERVICES"))
+      services = services["mysql-5.1"][0]["credentials"]
+      return MySQLdb.connect(services["hostname"], services["username"], services["password"], "test")
+
+  except:
+    return "Unexpected error:", sys.exc_info()[0]
 
 if __name__ == '__main__':
     app.run(debug=True)
